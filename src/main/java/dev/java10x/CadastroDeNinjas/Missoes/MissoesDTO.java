839d8cd471f63972e9a79ca268cb0fe218d9bd6f
Thiago <thiago.vascomarinhO@yahoo.com.br>
2026-07-03 22:38:10 -0300

    chore: Inclui a versão do DTO pelas entidades e mapeia manualmente no NinjaMapper e MissoesMapper, os caminhas entidade x DTO

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,17 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesDTO {
+
+    private Long id;
+    private String nomeMissao;
+    private String rank;
+    private List<NinjaModel> ninja;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,26 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO){
+
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNomeMissao(missoesDTO.getNomeMissao());
+        missoesModel.setRank(missoesDTO.getRank());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel){
+
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNomeMissao(missoesModel.getNomeMissao());
+        missoesDTO.setRank(missoesModel.getRank());
+
+        return missoesDTO;
+    }
 }
